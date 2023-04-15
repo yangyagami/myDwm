08a97c8431f23bbcbb61053b1ca7f2e16ef54613
yangyagami <a651685099@163.com>
2023-04-15 19:14:46 +0800

    修复脚本问题

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,6 +1,7 @@
 while true; 
 do
-	wallpaper=$(find $HOME/Pictures/Wallpapers -type f | shuf -n 1)
+	wallpaper=$(find $HOME/Pictures/Wallpapers/ -type f | shuf -n 1)
+	echo ${wallpaper}
 	feh ${wallpaper} --bg-scale
 	sleep 5m
 done
