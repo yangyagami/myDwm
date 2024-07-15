9a5d28db12305c9755c0988ccb4fc35e7713aea6
Siyu Yang <a651685099@163.com>
2024-07-15 09:51:02 +0800

    local commit

--- config.h
+++ config.h
@@ -98,7 +98,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_nordbg, "-nf", col_nordfg, "-sb", col_nordbgSel, "-sf", col_nordfgSel, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "fish", NULL };
 static const char *flameshotcmd[] = { "flameshot", "gui", NULL };
 
 static Key keys[] = {
