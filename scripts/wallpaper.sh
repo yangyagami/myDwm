f95660ce536fcdacf0e368c2c746c021f3fdd22e
yangsiyu <a651685099@163.com>
2024-07-21 19:31:01 +0800

    sync from local

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,6 +1,21 @@
-while true; 
-do
-	wallpaper=$(find $HOME/Pictures/Wallpapers/ -type f | shuf -n 1)
-	feh ${wallpaper} --bg-scale
-	sleep 1m
+#!/bin/bash
+
+# 存储文件名的数组
+file_array=()
+
+# 将文件夹下的所有文件名存储到数组中
+for file in $HOME/Pictures/Wallpapers/*; do
+    if [ -f "$file" ]; then
+        file_array+=("$(basename "$file")")
+    fi
 done
+
+while :
+do
+    # 遍历数组，设置背景图片
+    for filename in "${file_array[@]}"; do
+	feh $HOME/Pictures/Wallpapers/$filename --bg-scale
+	sleep 1m
+    done
+done
+
